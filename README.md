463e7194ebd60681fd5f98bdee10f2c01df8f682
fariscode511 <farisksa79@gmail.com>
2021-01-14 20:14:17 +0300

    New Docs

---
+++ README.md
@@ -0,0 +1,52 @@
+# PhpSanitization
+
+![](https://img.shields.io/packagist/l/phpsanitization/phpsanitization) ![](https://img.shields.io/packagist/dt/phpsanitization/phpsanitization) ![](https://img.shields.io/packagist/php-v/phpsanitization/phpsanitization) ![](https://img.shields.io/packagist/stars/phpsanitization/phpsanitization) ![](https://img.shields.io/packagist/v/phpsanitization/phpsanitization)
+
+## About
+Simple PHP Sanitization Class
+
+Support String, Arrays, and Associative Arrays
+
+## Features
+
+1. Out-Of-The-Box
+2. Support String, Arrays, and Associative Arrays
+3. Escape PDO and SQL queries
+4. Easy to Use
+
+## How to install
+
+```
+composer require phpsanitization/phpsanitization
+```
+
+## Usage
+
+### With Constructor
+```php
+include_once 'vendor/autoload.php';
+
+use PhpSanitization\PhpSanitization\Sanitization;
+
+$s = new Sanitization("<script>alert('xss');</script>");
+
+echo $s->esc();
+```
+
+### Without Constructor
+```php
+include_once 'vendor/autoload.php';
+
+use PhpSanitization\PhpSanitization\Sanitization;
+
+$s = new Sanitization();
+
+echo $s->esc("<script>alert('xss');</script>");
+```
+## License
+
+This project is distributed under the MIT license
+
+## Copyright
+
+Copyright (c) FarisCode - 2021
---
+++ docs/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\PhpSanitization\\Sanitization",
+            "name": "Sanitization",
+            "summary": "Simple\u0020PHP\u0020Sanitization\u0020Class",
+            "url": "classes/PhpSanitization-Sanitization.html"
+        },                {
+            "fqsen": "\\PhpSanitization\\Sanitization\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "PhpSanitization\u0020Class\u0020Constructor",
+            "url": "classes/PhpSanitization-Sanitization.html#method___construct"
+        },                {
+            "fqsen": "\\PhpSanitization\\Sanitization\u003A\u003Asanitize\u0028\u0029",
+            "name": "sanitize",
+            "summary": "Sanitize\u0020value",
+            "url": "classes/PhpSanitization-Sanitization.html#method_sanitize"
+        },                {
+            "fqsen": "\\PhpSanitization\\Sanitization\u003A\u003Aesc\u0028\u0029",
+            "name": "esc",
+            "summary": "Like\u0020Codeigniter\u0020sanatize\u0020an\u0020associative\u0020array,\u0020a\u0020sequential\u0020array\u0020or\u0020a\u0020string",
+            "url": "classes/PhpSanitization-Sanitization.html#method_esc"
+        },                {
+            "fqsen": "\\PhpSanitization\\Sanitization\u003A\u003AisAssoc\u0028\u0029",
+            "name": "isAssoc",
+            "summary": "Check\u0020if\u0020the\u0020provided\u0020array\u0020is\u0020an\u0020associative\u0020or\u0020a\u0020sequential\u0020array",
+            "url": "classes/PhpSanitization-Sanitization.html#method_isAssoc"
+        },                {
+            "fqsen": "\\PhpSanitization\\Sanitization\u003A\u003A\u0024data",
+            "name": "data",
+            "summary": "The\u0020value\u0020of\u0020the\u0020malicious\u0020string\u0020you\u0020want\u0020to\u0020sanitize",
+            "url": "classes/PhpSanitization-Sanitization.html#property_data"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\PhpSanitization",
+            "name": "PhpSanitization",
+            "summary": "",
+            "url": "namespaces/phpsanitization.html"
+        }            ]
+);
